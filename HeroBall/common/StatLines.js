fa5fcd644d3cd506b8ab6ea5052af7aaa44ed0b9
Matt <readspaceapp@gmail.com>
2019-08-14 20:54:28 +1000

    Include optional ranking for stat lines

--- HeroBall/common/StatLines.js
+++ HeroBall/common/StatLines.js
@@ -11,23 +11,44 @@ class StatLines extends React.Component {
         tableData={tableData} = array of remaining rows
         widthArr={widthArr} = int array for column width
         firstColumnData={firstColumnData} = array for first column (so it doesnt scroll)
+        showRanking= bool = optional = list the number next to each row
+        startingRank= int = optional = starting number for above
     */
-  
+    headerWidthArr = [150]
+    firstColumnTitles = []
+
+    constructor(props) {
+      super(props)
+
+      this.firstColumnTitles.push(props.firstColumnTitle)
+
+      if (this.props.showRanking) {
+        this.headerWidthArr.unshift(35)
+        this.firstColumnTitles.unshift('#')
+      }
+    }
+
   render() {
+
     return (
         <View style={{flexDirection:'row'}}>
           <Table borderStyle={{borderWidth: 2, borderColor: 'lightsteelblue'}}>
-            <Row widthArr={[150]} data={[this.props.firstColumnTitle]} style={styles.head} textStyle={styles.text}/>
-            {this.props.firstColumnData.map((rowData, index) => (
-                    <Row
-                      key={index}
-                      data={rowData}
-                      widthArr={[150]}
-                      style={[styles.row, index%2 && {backgroundColor: 'lightsteelblue'}]}
-                      textStyle={styles.text}
-                    />
-                  ))
-            }          
+            <Row widthArr={this.headerWidthArr} data={this.firstColumnTitles} style={styles.head} textStyle={styles.text}/>
+            {this.props.firstColumnData.map((rowData, index) => {
+              
+              if (this.props.showRanking === true) {
+                ranking = this.props.startingRank + index
+                rowData.unshift(ranking)
+              }
+                return (
+                        <Row
+                          key={index}
+                          data={rowData}
+                          widthArr={this.headerWidthArr}
+                          style={[styles.row, index%2 && {backgroundColor: 'lightsteelblue'}]}
+                          textStyle={styles.text}
+                        />
+                      )})}
           </Table>
         <ScrollView horizontal={true}>
           <Table borderStyle={{borderWidth: 2, borderColor: 'lightsteelblue'}}>
